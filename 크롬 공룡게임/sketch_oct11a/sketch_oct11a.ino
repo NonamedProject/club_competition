e3ec030aedfd849eada5f4865622e269473ce38c
godo1427 <tlgustlgus0425@naver.com>
2018-10-12 14:39:51 +0900

    Sample Code

---
+++ 크롬 공룡게임/sketch_oct11a/sketch_oct11a.ino
@@ -0,0 +1,30 @@
+#include "Keyboard.h"
+int state;
+int state2;
+ const byte btn = 2;
+ const byte btn2 = 3;
+void setup() {
+  // put your setup code here, to run once:
+  Keyboard.begin();
+ pinMode(btn, INPUT);
+  pinMode(btn2, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  state = digitalRead(btn);
+  state2 = digitalRead(btn2);
+  if(state==0)
+  {
+    Keyboard.write(' ');
+    delay(220);
+  }
+  if(state2==0)
+  {
+    Keyboard.press(217); 
+  }
+  else{
+  Keyboard.release(217);
+  }
+  
+}
